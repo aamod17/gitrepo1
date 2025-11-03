831426cfc53d84fdd72012a504c42ed6195e098f
amod <aamodpoddar@gmail.com>
2025-11-03 10:02:22 +0530

    added a package com.amodit.string.examples

---
+++ src/main/java/com/amodit/string/examples/StringUpperCaseDemo.java
@@ -0,0 +1,13 @@
+package com.amodit.string.examples;
+
+public class StringUpperCaseDemo {
+
+	public static void main(String[] args) {
+		
+		String name = "ramnivas";
+		String nameInUpperCase = name.toUpperCase();
+		System.out.println(nameInUpperCase);
+
+	}
+
+}
